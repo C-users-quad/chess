e710cb1e5e863da07bdd6579ffa3d7d887602ea5
C-users-quad <alexeatsburger@gmail.com>
2026-02-01 02:28:28 -0500

    mostly completed base game engine

--- src/settings.py
+++
@@ -1,7 +0,0 @@
-import pygame
-from os.path import join
-import sys
-
-WINDOW_SIZE = (800, 600)
-CLEAR_COLOR = "#68f66a"
-FPS = 60
--- src/test.py
+++
@@ -1,8 +0,0 @@
-stack = []
-
-stack.append(0)
-stack.append(1)
-stack.append(2)
-print(stack)
-stack.pop()
-print(stack)
