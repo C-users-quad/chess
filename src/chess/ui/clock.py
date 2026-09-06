6c1b1d6fde2dd20e052e6fcfbb168638c5f23f7f
C-users-quad <alexeatsburger@gmail.com>
2026-09-06 18:37:36 -0400

    Comment out UIClock class and related methods
    Comment out the UIClock class and its methods for future reference.

--- src/chess/ui/clock.py
+++ src/chess/ui/clock.py
@@ -1,58 +1,59 @@
-from core.enums import PieceColors, StateNames
-from core.settings import VariableSettings
-from ui.rects.text_box import UITextBox
+"""dont use this anymore lol"""
+# from core.enums import PieceColors, StateNames
+# from core.settings import VariableSettings
+# from ui.rects.text_box import UITextBox
 
 
-class UIClock(UITextBox):
-    def __init__(self, **kwargs):
-        """
-        displays the time remaining in minutes:seconds
-        for the player who is currently moving
-        """
-        super().__init__(text=self.curr_time_string, **kwargs)
-        self.prev_time_remaining = VariableSettings.player_time
+# class UIClock(UITextBox):
+#     def __init__(self, **kwargs):
+#         """
+#         displays the time remaining in minutes:seconds
+#         for the player who is currently moving
+#         """
+#         super().__init__(text=self.curr_time_string, **kwargs)
+#         self.prev_time_remaining = VariableSettings.player_time
 
-    @property
-    def time_remaining(self) -> int:
-        chess_state = self.game.get_state(StateNames.CHESS)
-        match chess_state.board.turn_color:
-            case PieceColors.WHITE:
-                return chess_state.white_time
-            case PieceColors.BLACK:
-                return chess_state.black_time
+#     @property
+#     def time_remaining(self) -> int:
+#         chess_state = self.game.get_state(StateNames.CHESS)
+#         match chess_state.board.turn_color:
+#             case PieceColors.WHITE:
+#                 return chess_state.white_time
+#             case PieceColors.BLACK:
+#                 return chess_state.black_time
 
-    @property
-    def curr_time_string(self):
+#     @property
+#     def curr_time_string(self):
 
-        if self.game.get_state(StateNames.CHESS) is None:
-            minutes = VariableSettings.player_time // 60
-            seconds = VariableSettings.player_time % 60
-        else:
-            minutes = self.time_remaining // 60
-            seconds = self.time_remaining % 60
+#         if self.game.get_state(StateNames.CHESS) is None:
+#             minutes = VariableSettings.player_time // 60
+#             seconds = VariableSettings.player_time % 60
+#         else:
+#             minutes = self.time_remaining // 60
+#             seconds = self.time_remaining % 60
 
-        # convert times to strings
-        minutes = str(minutes)
-        seconds = str(seconds)
+#         # convert times to strings
+#         minutes = str(minutes)
+#         seconds = str(seconds)
 
-        # format time strings if they are single-digit and need a zero
-        if len(minutes) < 2:
-            minutes = "0" + minutes
-        if len(seconds) < 2:
-            seconds = "0" + seconds
+#         # format time strings if they are single-digit and need a zero
+#         if len(minutes) < 2:
+#             minutes = "0" + minutes
+#         if len(seconds) < 2:
+#             seconds = "0" + seconds
 
-        return f"{minutes}:{seconds}"
+#         return f"{minutes}:{seconds}"
 
-    def detect_time_passing(self):
-        """
-        if the time remaining has changed,
-        re-render with new time string
-        """
-        if self.prev_time_remaining != self.time_remaining:
-            self.text = self.curr_time_string
-            self.dirty = True
+#     def detect_time_passing(self):
+#         """
+#         if the time remaining has changed,
+#         re-render with new time string
+#         """
+#         if self.prev_time_remaining != self.time_remaining:
+#             self.text = self.curr_time_string
+#             self.dirty = True
 
-        self.prev_time_remaining = self.time_remaining
+#         self.prev_time_remaining = self.time_remaining
 
-    def update(self):
-        self.detect_time_passing()
+#     def update(self):
+#         self.detect_time_passing()
